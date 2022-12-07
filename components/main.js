32c5204c0cbe755dea4e3bb6ca364d5eee1ece37
Marcin Kowalik <kowalikm@op.pl>
2022-12-07 20:11:42 +0100

    Add useState and axios api

--- components/main.js
+++ components/main.js
@@ -1,30 +1,97 @@
 import React, { useState, useEffect } from "react";
 import { View, Text, StyleSheet } from "react-native";
 import axios from "axios";
-import { ListItem, Badge} from '@rneui/themed'
+import { ListItem, Badge } from "@rneui/themed";
+
 const Main = () => {
+  const [data, setData] = useState({
+    Ok: 0,
+    Warning: 0,
+    Error: 0,
+    Timeout: 0,
+  });
 
-  const title = "0%";
-  const subtitle = "ggsgs0%"
+  const getData = async () => {
+    try {
+      const { data } = await axios.get(
+        `https://sprawdzanie-cen.rainbowtours.pl/api/sprawdzanie-cen-api/menu`
+      );
+      setData(data.Podsumowanie);
+    } catch (err) {
+      console.log("Problem z API " + err);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  console.log(data);
+
+  const values = Object.values(data);
+  const sum = values.reduce((accumulator, value) => {
+    return accumulator + value;
+  }, 0);
+
+  console.log(sum);
+
+  const percentOk = (100 * data.Ok) / sum;
+  console.log(percentOk.toFixed());
+
   return (
-    
-<View>
-
+    <View>
       <ListItem bottomDivider>
-      <Badge value="3" status="success" badgeStyle={{ padding: 16 }} textStyle={{ fontSize: 18 }}/>
+        <Badge
+          value={data.Error.toString()}
+          status="error"
+          badgeStyle={{ padding: 16 }}
+          textStyle={{ fontSize: 18 }}
+        />
         <ListItem.Content>
-          <ListItem.Title>{title}</ListItem.Title>
-          <ListItem.Subtitle>{subtitle}</ListItem.Subtitle>
+          <ListItem.Title>{`${data.Error} %`}</ListItem.Title>
+          {/*   <ListItem.Subtitle>{subtitle}</ListItem.Subtitle> */}
         </ListItem.Content>
       </ListItem>
-  
-
-</View>
+      <ListItem bottomDivider>
+        <Badge
+          value={data.Warning.toString()}
+          status="warning"
+          badgeStyle={{ padding: 16 }}
+          textStyle={{ fontSize: 18 }}
+        />
+        <ListItem.Content>
+          <ListItem.Title>{`${data.Warning} %`}</ListItem.Title>
+          {/*   <ListItem.Subtitle>{subtitle}</ListItem.Subtitle> */}
+        </ListItem.Content>
+      </ListItem>
+      <ListItem bottomDivider>
+        <Badge
+          value={data.Ok.toString()}
+          status="success"
+          badgeStyle={{ padding: 16 }}
+          textStyle={{ fontSize: 18 }}
+        />
+        <ListItem.Content>
+          <ListItem.Title>{`${percentOk.toFixed()} %`}</ListItem.Title>
+          {/*   <ListItem.Subtitle>{subtitle}</ListItem.Subtitle> */}
+        </ListItem.Content>
+      </ListItem>
+      <ListItem bottomDivider>
+        <Badge
+          value={data.Timeout.toString()}
+          status="primary"
+          badgeStyle={{ padding: 16 }}
+          textStyle={{ fontSize: 18 }}
+        />
+        <ListItem.Content>
+          <ListItem.Title>{`${data.Timeout} %`}</ListItem.Title>
+          {/*   <ListItem.Subtitle>{subtitle}</ListItem.Subtitle> */}
+        </ListItem.Content>
+      </ListItem>
+    </View>
   );
 };
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
 
 export default Main;
